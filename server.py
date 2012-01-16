6f9445cc51a607a51d7182061ec302ec80f7ad5d
Alexey O <aor88f@gmail.com>
2012-01-16 09:31:13 +0400

    Add lock

--- server.py
+++ server.py
@@ -1,37 +1,40 @@
 import asyncore
+import threading
 import socket
 import time
 import substitutor3000
 
-_sbst = substitutor3000.Substitutor3000()
 
 class Handler(asyncore.dispatcher_with_send):
-    sleep = 0
-    
+    _sbst = substitutor3000.Substitutor3000()
+    _sleep = 0
+    _lock = threading.Lock()
+
     def handle_read(self):
-        
         data = self.recv(8192).rstrip()
-        time.sleep(self.sleep)
+        time.sleep(self._sleep)
         if data:
             com, param = self._sep(data)
             param, param2 = self._sep(param)
             if com == 'GET':
+                self._lock.acquire()
                 self.send('VALUE\n')
-                self.send(_sbst.get(param) + '\n')
+                self.send(self._sbst.get(param) + '\n')
+                self._lock.release()
             elif com == 'PUT':
+                self._lock.acquire()
                 self.send('OK\n')
-                _sbst.put(param, param2)
+                self._sbst.put(param, param2)
+                self._lock.release()
             elif com == 'SET':
                 if param == 'SLEEP':
                     self.send('OK\n')
                     try:
-                        self.sleep = int(param2) / 1000.0
-                        if self.sleep < 0:
+                        self._sleep = int(param2) / 1000.0
+                        if self._sleep < 0:
                             raise Exception()
                     except Exception:
-                        self.sleep = 0
-                    
-            
+                        self._sleep = 0
         self.close()
 
     def _sep(self, s):
