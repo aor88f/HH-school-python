476321cd0abc62cf59c829100ebefbbfb84207dd
Alexey O <aor88f@gmail.com>
2012-01-17 04:29:10 +0400

    Change BaseFunctionalTest to ServerBg

---
+++ serverbg.py
@@ -0,0 +1,26 @@
+import threading
+import server
+
+class ServerBg(threading.Thread):
+    _lock = threading.Lock()
+    _port = 23232
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def __del__(self):
+        self._lock.release()
+        pass
+
+    def port(self):
+        return self._port
+
+    def run(self):
+        self._lock.acquire()
+        server.Server('', self._port).run()
+        self._lock.release()
+
+
+if __name__ == "__main__":
+    ServerBg().start()
+    print ":)"
