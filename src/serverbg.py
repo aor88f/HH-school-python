c795816a6db27a7156ae339a42004a86b9f23d41
Alexey O <aor88f@gmail.com>
2012-01-26 12:04:46 +0400

    Remove locks from serverbg

--- src/serverbg.py
+++ src/serverbg.py
@@ -1,8 +1,7 @@
 import threading
 import server
 
-class ServerBg(object, threading.Thread):
-    _lock = threading.Lock()
+class ServerBg(threading.Thread):
     _port = 23232
 
     def __init__(self):
@@ -10,16 +9,13 @@ class ServerBg(object, threading.Thread):
         threading.Thread.__init__(self)
 
     def __del__(self):
-        self._lock.release()
         pass
 
     def port(self):
         return self._port
 
     def run(self):
-        self._lock.acquire()
         server.Server('', self._port).run()
-        self._lock.release()
 
 
 if __name__ == "__main__":
