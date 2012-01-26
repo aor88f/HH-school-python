99e82f2a3cf0335994a736a6cc9ff56b21af0ee4
Alexey O <aor88f@gmail.com>
2012-01-26 11:49:26 +0400

    Rename tests

---
+++ test/serverresponse_test.py
@@ -0,0 +1,66 @@
+import unittest
+import serverbg
+
+
+class ServerResponseTest(unittest.TestCase):
+    _serverbg = None
+
+    def __init__(self, methodName='runTest'):
+        unittest.TestCase.__init__(self, methodName)
+        self._serverbg = serverbg.ServerBg()
+        self._serverbg.start()
+
+    def testPutGet(self):
+        import socket
+        host = 'localhost'
+        port = self._serverbg.port()
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send("PUT K1 one")
+        self.assertEqual("OK\n", recv(s))
+        s.close()
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send("GET K1")
+        self.assertEqual("VALUE\none\n", recv(s))
+        s.close()
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send("PUT keys ${K1} ${K2}")
+        self.assertEqual("OK\n", recv(s))
+        s.close()
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send("GET keys")
+        self.assertEqual("VALUE\none \n", recv(s))
+        s.close()
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send("PUT K2 two")
+        self.assertEqual("OK\n", recv(s))
+        s.close()
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send("SET SLEEP 1")
+        self.assertEqual("OK\n", recv(s))
+        s.close()
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send("GET keys")
+        self.assertEqual("VALUE\none two\n", recv(s))
+        s.close()
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
+
+def recv(s):
+    return s.recv(8192)
--- test/substitutor3000_test.py
+++ test/substitutor3000_test.py
@@ -2,15 +2,15 @@ import unittest
 import substitutor3000
 
 
-class TestSubstitutor3000(unittest.TestCase):
-    def testReplace1(self):
+class Substitutor3000Test(unittest.TestCase):
+    def testVar(self):
         sbst = substitutor3000.Substitutor3000();
         sbst.put("k1", "one");
         sbst.put("k2", "two");
         sbst.put("keys", "1: ${k1}, 2: ${k2}");        
         self.assertEqual("1: one, 2: two", sbst.get("keys"));
 
-    def testReplace2(self):
+    def testSubVar(self):
         sbst = substitutor3000.Substitutor3000();
         sbst.put("X", "ONE");
         sbst.put("Y", "TWO");
